a1375e315abc0081c4bb025bfd20aaa43236f1fe
ahsantamim <ahsantamim49@gmail.com>
2024-09-14 11:23:22 +0600

    config

---
+++ server/config/config.ts
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const config = {
+  development: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+  },
+};
+
+export default config;
