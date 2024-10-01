97c9dec3d106cf8d3a797eef54c5772bc7cf9ef0
Abeda Khanam <abeda2152@gmail.com>
2024-10-01 11:13:35 +0600

    done

--- server/src/routes/commentRoutes.ts
+++ server/src/routes/commentRoutes.ts
@@ -1,21 +1,21 @@
 // src/routes/commentRoutes.ts
-import { Router } from 'express';
+import { Router } from "express";
 import {
   createComment,
   updateComment,
   deleteComment,
-} from '../controllers/commentController';
-import { authenticateToken } from '../middlewares/authMiddleware';
+} from "../controllers/commentController";
+import { authenticateToken } from "../middlewares/authMiddleware";
 
 const router = Router();
 
 // Protected route for adding a comment to a specific article
-router.post('/articles/:articleId/comments', authenticateToken, createComment);
+router.post("/articles/:articleId/comments", authenticateToken, createComment);
 
 // Protected route for updating a specific comment
-router.put('/comments/:commentId', authenticateToken, updateComment);
+router.put("/comments/:commentId", authenticateToken, updateComment);
 
 // Protected route for deleting a specific comment
-router.delete('/comments/:commentId', authenticateToken, deleteComment);
+router.delete("/comments/:commentId", authenticateToken, deleteComment);
 
 export default router;
